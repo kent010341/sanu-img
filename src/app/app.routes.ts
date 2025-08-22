397ee582e832b2c47a9064f71d457a0ba5e04d99
Kent010341 <kent010341@gmail.com>
2025-08-22 23:45:01 +0800

    #3 add path config; add route path and router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EditorWorkspace } from './layout/editor-workspace/editor-workspace';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: EditorWorkspace }
+];
